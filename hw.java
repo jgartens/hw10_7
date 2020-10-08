c0a36a9ee7e4e150def73e38f339a014bcea2e56
Jacob Gartenstein <jake@Jacobs-MacBook-Pro.local>
2020-10-08 08:15:32 -0500

    changes to switch

--- hw.java
+++ hw.java
@@ -42,19 +42,15 @@ public class hw {
 
     //part 2: 1.
     public static char getLetterGradeSwitch(double numGrade){
-        switch ((int)numGrade){
+        switch (((int)numGrade)/ 10){
 
-            case 100:
-            case 99:
-            case 98:
-            case 97:
-            case 96: case 95: case 94: case 93: case 92: case 91: case 90:
+            case 10: case 9:
                 return 'A';
-            case 89: case 88: case 87: case 86: case 85: case 84: case 83: case 82: case 81: case 80:
+            case 8:
                 return 'B';
-            case 79: case 78: case 77: case 76: case 75: case 74: case 73: case 72: case 71: case 70:
+            case 7:
                 return 'C';
-            case 69: case 68: case 67: case 66: case 65: case 64: case 63: case 62: case 61: case 60:
+            case 6:
                 return 'D';
             default: 
                 return 'F';
